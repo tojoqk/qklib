9ba39922192862629198c7d4a9fd1afbdb650de0
Masaya Tojo <masaya@tojo.tokyo>
2025-01-09 02:02:11 +0900

    Add "typed-racket-lib" to deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "qklib")
-(define deps '("base"))
+(define deps '("base" "typed-racket-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/qklib.scrbl" ())))
 (define pkg-desc "Description Here")
